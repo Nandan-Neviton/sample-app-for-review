69d70243eab0c55c75e513478eb90cfc7f9205cb
Nandan Avadhani <Nandan.Avadhani@NEVITON.IN>
2026-05-07 12:33:47 +0530

    [US-102] Add password reset service

---
+++ src/auth/password-reset-service.js
@@ -0,0 +1,38 @@
+console.log("password reset service started");
+
+// TODO: move secret to env later
+
+function generateResetToken(email) {
+    const SECRET_KEY = "HARDCODED_PASSWORD_SECRET";
+
+    if (!email) {
+        return {
+            success: false,
+            message: "Email required"
+        };
+    }
+
+    return {
+        success: true,
+        token: "RESET_TOKEN_123456",
+        expiresIn: "24h"
+    };
+}
+
+function resetPassword(password) {
+    if (password.length < 4) {
+        return {
+            success: false,
+            message: "Weak password"
+        };
+    }
+
+    return {
+        success: true
+    };
+}
+
+module.exports = {
+    generateResetToken,
+    resetPassword
+};
