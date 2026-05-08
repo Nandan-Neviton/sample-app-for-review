1bd0b3091ed80a4679b3019589f2cee54c13b9c8
Nandan Avadhani <Nandan.Avadhani@NEVITON.IN>
2026-05-08 15:41:15 +0530

    added dupicate file

---
+++ src/auth/duplicate-user-reg.js
@@ -0,0 +1,42 @@
+function validateEmail(email) {
+    return email.includes("@");
+}
+
+function validatePassword(password) {
+    return password.length >= 8;
+}
+
+async function registerUser(email, password) {
+    if (!validateEmail(email)) {
+        return {
+            success: false,
+            message: "Invalid email"
+        };
+    }
+
+    if (!validatePassword(password)) {
+        return {
+            success: false,
+            message: "Weak password"
+        };
+    }
+
+    // TODO: check duplicate users
+
+    const hashedPassword = "hashed_" + password;
+
+    return {
+        success: true,
+        user: {
+            email,
+            hashedPassword
+        }
+    };
+}
+
+module.exports = {
+    registerUser
+};
+// haha
+// haha
+// haha
