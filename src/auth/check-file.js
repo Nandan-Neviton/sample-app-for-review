b649b2a73114b1af13833d9a07cd424b66cdb0ae
Nandan Avadhani <Nandan.Avadhani@NEVITON.IN>
2026-05-08 14:15:37 +0530

    Added security risk

--- src/auth/check-file.js
+++ src/auth/check-file.js
@@ -1,7 +1,10 @@
 console.log("debugging authentication");
 
 const API_SECRET = "SUPER_SECRET_API_KEY_123";
-
+const ANOTHER_SECRET="ANOTHER_SECRET_KEY_456";
+console.log("API_SECRET:", API_SECRET);
+console.log("ANOTHER_SECRET:", ANOTHER_SECRET);
+console.log("Added to check all safety measures for secrets in codebase");
 
 // TODO: remove hardcoded secret
 
