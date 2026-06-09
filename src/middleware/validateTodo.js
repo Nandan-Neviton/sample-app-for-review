d4f37bca49cbb62338731604c5de1e9bb92b949e
Nandan Avadhani <Nandan.Avadhani@NEVITON.IN>
2026-06-09 10:25:59 +0530

    test-for new url

--- src/middleware/validateTodo.js
+++ src/middleware/validateTodo.js
@@ -42,3 +42,37 @@ module.exports = validateTodo;
 // test
 //test 
 //t e s t
+
+//   if (typeof title !== "string") {
+//     return res.status(400).json({
+//       success: false,
+//       message: "Title must be a string"
+//     });
+//   }
+
+//   if (title.trim().length === 0) {
+//     return res.status(400).json({
+//       success: false,
+//       message: "Title cannot be empty"
+//     });
+//   }
+
+//   if (title.length > 100) {
+//     return res.status(400).json({
+//       success: false,
+//       message: "Title cannot exceed 100 characters"
+//     });
+//   }
+
+//   next();
+// }
+
+// module.exports = validateTodo;
+// //
+// //breakthrough envision done by me
+// // this is test on new branch
+// // to check if the works in the 
+// // dashboard or not.
+// // test
+// // test
+// //test 
